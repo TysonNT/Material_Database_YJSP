5f731370ffea11dc73cdee4d18c9ee11e85c192e
TysonNT <tyson26tran@gmail.com>
2026-03-23 23:01:13 -0700

    Update dashboard.py

--- matproplib/src/matproplib/dashboard.py
+++ matproplib/src/matproplib/dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matprotlib as mp
+import matproplib as mp
 
 # --- 1. Page Setup ---
 st.set_page_config(page_title="Material Explorer", layout="wide")
